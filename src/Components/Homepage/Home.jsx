9b6d42ac531268e3567e7deb6c96870458b85bd1
Mehedi Hassan <mh93075@gmail.com>
2024-01-09 13:43:03 +0600

    first commit

---
+++ src/Components/Homepage/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Hello This is Home</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Home from './Components/Homepage/Home.jsx';
+import Layout from './Components/Layout/Layout.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
